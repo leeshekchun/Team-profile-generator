e042abd14d063c0c14e8341bb14a7956da9c5663
ShekChun Lee <leeshekchun@gmail.com>
2022-05-19 09:35:47 -0600

    Employee parent class with properties and methods

---
+++ dist/Employee.js
@@ -0,0 +1,27 @@
+// Employee parent class with properties
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+// Employee parent class with methods
+    getName() {
+        return this.name;
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return Employee
+    }
+}
+
+module.exports = Employee;
