dffdab856cfb74605e582c0504736809579f8bea
ShekChun Lee <leeshekchun@gmail.com>
2022-05-19 09:55:59 -0600

    install inquirer and jest packages

---
+++ index.js
@@ -0,0 +1,7 @@
+// getting packages and modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
